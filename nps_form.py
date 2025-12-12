89d2cf18a0323873ce2c2dd1a485a8b3f349f060
jeraa63-cmyk <jeraa63@gmail.com>
2025-12-12 14:50:30 -0300

    Update nps_form.py

--- nps_form.py
+++ nps_form.py
@@ -431,7 +431,7 @@ if step == 1:
     if LOGO_FULL.exists():
         st.markdown(
             f"<img alt='Jera' src='{_img_data_uri(LOGO_FULL)}' "
-"style='display:block;margin:-28px auto 0 auto;width:480px;max-width:95%;'/>",
+"style='display:block;margin:-36px auto 0 auto;width:480px;max-width:95%;'/>",
 
             unsafe_allow_html=True,
         )
@@ -633,4 +633,5 @@ st.markdown("</div>", unsafe_allow_html=True)
 
 # -------- RODAPÉ FIXO --------
 st.markdown("<div class='footer-fixed'>© Jera Capital — Todos os direitos reservados.</div>", unsafe_allow_html=True)
+
 
